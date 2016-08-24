aa7fb8a77dba71c6a92d5c431952509f1a2ab7b6
Alex Bezpalko <alexbe@wix.com>
2016-08-24 13:55:55 +0300

    use amazon nat and add packer subnet

--- terraform/staging.tf
+++ terraform/staging.tf
@@ -9,7 +9,7 @@ variable "grid_vpc_subnet" { default = {} }
 variable "main_vpc" { default = {} }
 variable "main_chef_environment" { default = {} }
 variable "grid_chef_environment" { default = {} }
-variable "count" {default = "1" }
+variable "count" {default = "2" }
 variable "office_networks" {
   default = [ "10.37.0.0/24", "172.28.0.0/14", "192.168.28.0/22", "192.168.36.0/22", "192.168.40.0/21", "192.168.199.0/24" ]
 }
@@ -35,6 +35,16 @@ resource "aws_internet_gateway" "wix-staging-ig" {
     }
 }
 
+resource "aws_eip" "nat" {
+    vpc   = true
+}
+
+resource "aws_nat_gateway" "nat" {
+  subnet_id     = "${aws_subnet.wix-staging-pub-subnet.id}"
+  allocation_id = "${aws_eip.nat.id}"
+  depends_on    = ["aws_internet_gateway.wix-staging-ig"]
+}
+
 ############# SUBNETS ##################
 # this section uses another unset variable is - az for availability zone
 resource "aws_subnet" "wix-staging-main-subnet" {
@@ -46,15 +56,25 @@ resource "aws_subnet" "wix-staging-main-subnet" {
         "Name" = "wix-staging subnet"
     }
 }
-resource "aws_subnet" "wix-staging-internal-subnet" {
+resource "aws_subnet" "wix-staging-pub-subnet" {
     vpc_id                  = "${aws_vpc.wix-staging.id}"
-    cidr_block              = "10.100.100.0/24"
+    cidr_block              = "10.100.1.0/24"
     availability_zone       = "us-west-2b"
     map_public_ip_on_launch = false
     tags {
-        "Name" = "wix-staging internal subnet"
+        "Name" = "wix-staging pub subnet"
     }
 }
+resource "aws_subnet" "wix-packer" {
+    vpc_id                  = "${aws_vpc.wix-staging.id}"
+    cidr_block              = "10.100.2.0/24"
+    # availability_zone       = "us-west-2b"
+    map_public_ip_on_launch = false
+    tags {
+        "Name" = "wix-packwer subnet"
+    }
+}
+
 ############## SECURITY GROUPS ##############
 resource "aws_security_group" "wix-staging-main-sg" {
     name        = "Main SG"
@@ -89,7 +109,7 @@ resource "aws_instance" "staging-instance" {
     key_name                    = "zozo"
     subnet_id                   = "${aws_subnet.wix-staging-main-subnet.id}"
     vpc_security_group_ids      = ["${aws_security_group.wix-staging-main-sg.id}"]
-    associate_public_ip_address = true
+    associate_public_ip_address = false
     # private_ip                  = "${lookup(var.grid_vpc_subnet, var.region)}${255-lookup(var.zone_first, var.az)}.254"
     source_dest_check           = 0
     disable_api_termination     = false
@@ -109,7 +129,7 @@ resource "null_resource" "staging-instance" {
         staging_instance_id = "${aws_instance.staging-instance.*.id[count.index]}"
     }
     connection={
-        host="${aws_instance.staging-instance.*.public_ip[count.index]}"
+        host="${aws_instance.staging-instance.*.private_ip[count.index]}"
         user="admin"
         private_key="${file("~/.ssh/zozo.pem")}"
         port="22"
@@ -117,7 +137,7 @@ resource "null_resource" "staging-instance" {
 
     #add external ip of the pfsence node to chef iptables so it will allow access for chef
     provisioner "local-exec" {
-        command = "echo ${aws_instance.staging-instance.*.public_ip[count.index]} node-${format("%02d", count.index)}.staging.wixpress.com"
+        command = "echo ${aws_instance.staging-instance.*.private_ip[count.index]} node-${format("%02d", count.index)}.staging.wixpress.com"
     }
 
     provisioner "local-exec" {
@@ -129,12 +149,13 @@ resource "null_resource" "staging-instance" {
     }
 
     # open iptables on chef
-    provisioner "local-exec" {
-        command = "ssh chef.wixpress.com 'sudo iptables -A INPUT -s ${aws_instance.staging-instance.*.public_ip[count.index]}/32 -p tcp -m multiport --dports 80,443 -j ACCEPT'"
-    }
+    # provisioner "local-exec" {
+    #     command = "ssh chef.wixpress.com 'sudo iptables -A INPUT -s ${aws_instance.staging-instance.*.private_ip[count.index]}/32 -p tcp -m multiport --dports 80,443 -j ACCEPT'"
+    # }
     provisioner "remote-exec" {
       inline = [
-        "echo ${aws_instance.staging-instance.*.public_ip[count.index]} node-${format("%02d",count.index)}.staging.wixpress.com",
+        "echo ${aws_instance.staging-instance.*.private_ip[count.index]} node-${format("%02d",count.index)}.staging.wixpress.com",
+        "sudo sh -c '/bin/echo -e \"172.16.209.188 chef.wixpress.com\n172.16.213.71 mirror.wixpress.com\" >> /etc/hosts'",
         "sudo sh -c 'hostname node-${format("%02d",count.index)}.staging.wixpress.com ; echo node-${format("%02d",count.index)}.staging.wixpress.com > /etc/hostname'",
         "sudo rm /etc/chef/client.pem",
         "sudo apt-get update"
@@ -160,10 +181,14 @@ resource "aws_route_table" "wix-staging-rt" {
 
     route {
         cidr_block = "0.0.0.0/0"
-        gateway_id = "${aws_internet_gateway.wix-staging-ig.id}"
+        nat_gateway_id = "${aws_nat_gateway.nat.id}"
     }
     route {
       cidr_block = "172.16.0.0/16"
+      gateway_id = "${aws_vpn_gateway.staging_vpn_gw.id}"
+    }
+    route {
+      cidr_block = "192.168.0.0/16"
       gateway_id = "${aws_vpn_gateway.staging_vpn_gw.id}"
     }
 
@@ -173,21 +198,25 @@ resource "aws_route_table" "wix-staging-rt" {
         "Name" = "wix-staging routing table"
     }
 }
-resource "aws_route_table_association" "wix-stagingdefault-route" {
-    route_table_id = "${aws_route_table.wix-staging-rt.id}"
-    subnet_id = "${aws_subnet.wix-staging-main-subnet.id}"
-}
+resource "aws_route_table" "wix-staging-pub" {
+    vpc_id     = "${aws_vpc.wix-staging.id}"
 
-# resource "aws_network_interface" "staging-instance-eni" {
-#     count             = "${var.count}"
-#     subnet_id         = "${aws_subnet.wix-staging-internal-subnet.id}"
-#     description       = "Internal network interface"
-#     source_dest_check = false
-#     attachment {
-#         instance     = "${element(aws_instance.staging-instance.*.id, count.index)}"
-#         device_index = 1
-#     }
-# }
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.wix-staging-ig.id}"
+    }
+    tags {
+        "Name" = "wix-staging pub routing table"
+    }
+}
+resource "aws_main_route_table_association" "wix-stagingdefault-route" {
+    route_table_id = "${aws_route_table.wix-staging-rt.id}"
+    vpc_id = "${aws_vpc.wix-staging.id}"
+}
+resource "aws_route_table_association" "wix-staging-pub" {
+    route_table_id = "${aws_route_table.wix-staging-pub.id}"
+    subnet_id = "${aws_subnet.wix-staging-pub-subnet.id}"
+}
 
 resource "aws_route53_zone" "ptr" {
   name = "0.100.10.in-addr.arpa"
@@ -217,23 +246,6 @@ resource "aws_route53_record" "staging" {
   zone_id   = "${aws_route53_zone.staging.id}"
   records   = ["${element(aws_instance.staging-instance.*.private_ip, count.index)}"]
 }
-
-# resource "aws_route53_record" "ptr-eni" {
-#   count     = "${var.count}"
-#   name      = "${format("%s.%s.100.10.in-addr.arpa", element(split(".", element(aws_network_interface.staging-instance-eni.*.private_ips[count.index],0)),3), element(split(".", element(aws_network_interface.staging-instance-eni.*.private_ips[count.index],0)),2))}"
-#   ttl       = "300"
-#   type      = "PTR"
-#   zone_id   = "${aws_route53_zone.ptr.id}"
-#   records   = ["${format("node-%02d-eni.staging.wixpress.com", count.index + 1)}"]
-# }
-# resource "aws_route53_record" "staging-eni" {
-#   count     = "${var.count}"
-#   name      = "${format("node-%02d-eni.staging.wixpress.com", count.index + 1)}"
-#   ttl       = "300"
-#   type      = "A"
-#   zone_id   = "${aws_route53_zone.staging.id}"
-#   records    = ["${aws_network_interface.staging-instance-eni.*.private_ips[count.index]}"]
-# }
 
 resource "aws_vpn_gateway" "staging_vpn_gw" {
   vpc_id    = "${aws_vpc.wix-staging.id}"
